4349c20e0d21996cb7237e31b0ff6229e21f57c9
Methu-v1 <methubot@gmail.com>
2025-05-22 12:10:59 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,7 +12,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `╭━━〔 *MD* 〕━━┈⊷
+let status = `╭━━〔 *METHU MD* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*: ${runtime(process.uptime())}
 ┃◈┃• *📟 Ram*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
