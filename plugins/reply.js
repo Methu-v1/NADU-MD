22c786495629e86116605b5b7b049f53736ba346
Methu-v1 <methubot@gmail.com>
2025-05-24 18:18:41 +0530

    Update and rename auto=reply+sticker+voice.js to reply.js

---
+++ plugins/reply.js
@@ -0,0 +1,23 @@
+//Nadu
+
+const fs = require('fs');
+const path = require('path');
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+//auto reply 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const data = await fetchJson(`https://raw.githubusercontent.com/Methu-v1/sudu/refs/heads/main/my_data/autoreply.json`)
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            if (config.AUTO_REPLY === 'true') {
+                if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'Queen Nethu Md'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
