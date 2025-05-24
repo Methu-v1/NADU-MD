c8b2ecfe4dc60acff9327b314eaccace131904cd
Methu-v1 <methubot@gmail.com>
2025-05-24 18:20:28 +0530

    Update reply.js

--- plugins/reply.js
+++ plugins/reply.js
@@ -10,7 +10,7 @@ cmd({
   on: "body"
 },    
 async (conn, mek, m, { from, body, isOwner }) => {
-    const data = await fetchJson(`https://raw.githubusercontent.com/Methu-v1/sudu/refs/heads/main/my_data/autoreply.json`)
+    const data = await fetchJson(`https://raw.githubusercontent.com/Methu-v1/NADU-MD/refs/heads/main/lib/my_data/autoreply.json`)
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             if (config.AUTO_REPLY === 'true') {
